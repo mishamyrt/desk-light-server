9a3c3640edfa36a12d89c6e5969da04b3439f305
mishamyrt <misha@myrt.co>
2022-03-20 10:47:08 +0400

    Send payload on ping

--- src/ping.ts
+++ src/ping.ts
@@ -1,9 +1,11 @@
+import { randomBytes } from 'crypto'
 import { performance } from 'perf_hooks'
 import { argv } from 'process'
 import { createClient } from './modules/client'
 import { createLog } from './modules/log'
 
 const REQUEST_COUNT = 1000
+const PAYLOAD_LENGTH = 24 * 3 // 24 colors
 const { log, err } = createLog('Ping')
 
 function averageValue (values: number[]): number {
@@ -28,11 +30,12 @@ async function main () {
   let successCount = 0
   let startTime = 0
   log(`Sending ${REQUEST_COUNT} requests to ${host}`)
+  log(`Payload length is ${PAYLOAD_LENGTH} bytes`)
   for (let i = 0; i < REQUEST_COUNT; i++) {
     startTime = performance.now()
     response = await client.send({
-      cmd: 'power_on',
-      args: []
+      cmd: 'send_raw',
+      args: [255, ...randomBytes(PAYLOAD_LENGTH)]
     })
     responseTime.push(performance.now() - startTime)
     if (response?.status === 'success') {
@@ -42,7 +45,7 @@ async function main () {
   await client.close()
   const averageTime = averageValue(responseTime).toFixed(2)
   const loss = calculateLoss(REQUEST_COUNT, successCount).toFixed(2)
-  log(`Average request time ${averageTime} ms, ${loss}% request loss`)
+  log(`Average response time ${averageTime} ms, ${loss}% request loss`)
 }
 
 main()
